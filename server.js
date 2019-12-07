bf357c7ff1d4b8da20aa22c7769793d7419fca6b
kkarunia23 <kenneth.karunia@gmail.com>
2019-12-07 17:48:33 +0800

    about updated

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const app = express();
 const db = mongoose.connection;
 
 // Environment Variables
-const mongoURI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/merncrud";
+// const mongoURI =
+//   process.env.MONGODB_URI || "mongodb://localhost:27017/merncrud";
 const PORT = process.env.PORT || 3000;
 
 // Connect to Mongo
-mongoose.connect(mongoURI, { useNewUrlParser: true }, () =>
-  console.log("MongoDB connection established:", mongoURI)
-);
+// mongoose.connect(mongoURI, { useNewUrlParser: true }, () =>
+//   console.log("MongoDB connection established:", mongoURI)
+// );
 
 // Error / Disconnection
 db.on("error", err => console.log(err.message + " is Mongod not running?"));
