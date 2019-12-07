0e492a64dc885ed22320567020464d4bcde8ab8b
daryl <test@gmail.com>
2019-12-07 10:30:19 +0800

    server.js created

---
+++ server.js
@@ -0,0 +1,41 @@
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const db = mongoose.connection;
+
+// Environment Variables
+const mongoURI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/merncrud";
+const PORT = process.env.PORT || 3000;
+
+// // Connect to Mongo
+// mongoose.connect(mongoURI, { useNewUrlParser: true }, () =>
+//   console.log("MongoDB connection established:", mongoURI)
+// );
+
+// // Error / Disconnection
+// db.on("error", err => console.log(err.message + " is Mongod not running?"));
+// db.on("disconnected", () => console.log("mongo disconnected"));
+
+// // Middleware
+// app.use(express.json()); // returns middleware that only parses JSON
+
+// app.use(express.static("public"));
+
+// // Routes
+// const todosController = require("./controllers/todos.js");
+// app.use("/todos", todosController);
+
+// // this will catch any route that doesn't exist
+// app.get("*", (req, res) => {
+//   res.status(404).json("Sorry, page not found");
+// });
+
+app.get("/", (req, res) => {
+  res.send("hello jello");
+});
+
+app.listen(PORT, () => {
+  console.log("Let's get things done on port", PORT);
+});
