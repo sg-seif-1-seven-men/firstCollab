5e6f4831fdcb081b04d59567b99fda1ff6f808f5
daryl <darylpoon@gmail.com>
2019-12-10 21:27:46 +0800

    completed create commitments component

--- models/commitments.js
+++ models/commitments.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commitmentsSchema = mongoose.Schema({
-  commitment: String,
+  commitment: ["Exercise once a week", "Quit Smoking"],
+  owner: [{ type: Schema.Types.ObjectId, ref: "Users" }],
   buddy: [{ type: Schema.Types.ObjectId, ref: "Users" }],
   referee: [{ type: Schema.Types.ObjectId, ref: "Users" }],
   success: Boolean,
-  progress: []
+  progress: ["Started", "On Track", "Need Help"]
 });
 
 const Commitments = mongoose.model("Commitments", commitmentsSchema);
