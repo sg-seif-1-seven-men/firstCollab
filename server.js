cdc30dcbddd44fd7c6c49d92f6f5080796b381e6
kkarunia23 <kenneth.karunia@gmail.com>
2019-12-11 21:30:54 +0800

    update pwd

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 const mongoURI =
   process.env.MONGODB_URI ||
-  'mongodb+srv://kkarunia23:lalaland123@cluster0-knxzn.mongodb.net/sticky?retryWrites=true&w=majority';
+  'mongodb+srv://kkarunia23:generalassembly123@cluster0-knxzn.mongodb.net/sticky?retryWrites=true&w=majority';
 
 // Connect to Mongo
 mongoose.connect(mongoURI, {
